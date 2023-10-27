3db8c08a301217534bf4919237a63180c4962b60
tuankietdang52 <dkiet27@gmail.com>
2023-10-27 17:14:09 +0700

    test2

--- js/code.js
+++ js/code.js
@@ -2,7 +2,7 @@ let product_click = false;
 let donhang_click = false;
 
 function default_click_navi(){
-    let navigate_detail = document.getElementById("navigate-detail");
+    let navigate_detail = document.getElementsByClassName("navigation navigation-detail")[0];
 
     navigate_detail.innerHTML = "";
     product_click = false;
@@ -14,7 +14,7 @@ function product_click_navi(){
 
     donhang_click = false;
     product_click = true;
-    let navigate_detail = document.getElementById("navigate-detail");
+    let navigate_detail = document.getElementsByClassName("navigation navigation-detail")[0];
 
     navigate_detail.innerHTML = "<div><li>Phone</li></div>" +
                                 "<div><li>Earphone/Earbuds</li></div>" +
@@ -26,7 +26,7 @@ function donhang_click_navi(){
 
     product_click = false;
     donhang_click = true;
-    let navigate_detail = document.getElementById("navigate-detail");
+    let navigate_detail = document.getElementsByClassName("navigation navigation-detail")[0];
 
     navigate_detail.innerHTML = "<div><li>Cho xet duyet</li></div>" +
                                 "<div><li>Dang giao hang</li></div>" +
