e8cece72d5824568b4b88f1082596cf7511c7548
tuankietdang52 <dkiet27@gmail.com>
2023-11-03 14:16:41 +0700

    update navigation section

--- js/code.js
+++ js/code.js
@@ -1,3 +1,21 @@
+function naviactive(pageactive){
+    if (pageactive.className == "navi-active") return;
+
+    let nonactive = document.getElementsByClassName("navigation")[0];
+
+    for (let i = 0; i < nonactive.children.length; i++){
+        let nonactivepage = nonactive.children[i];
+        if (nonactivepage.children[0].className == "navi-active"){
+            nonactivepage.children[0].className = "navi-nonactive";
+        }
+    }
+
+    pageactive.className = "navi-active";
+}
+
+
+
+
 function activeoption(pageactive){
     if (pageactive.className == "active") return;
 
