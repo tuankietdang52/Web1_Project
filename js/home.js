854cadf99bd7e908600533712339ff16a422851b
tuankietdang52 <dkiet27@gmail.com>
2023-11-05 23:32:27 +0700

    update slideshow

---
+++ js/home.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    var owl = $('.owl-carousel');
+	owl.owlCarousel({
+		items: 1.5,
+		margin: 100,
+		center: true,
+		loop: true,
+		smartSpeed: 450,
+		autoplay: true,
+		autoplayTimeout: 3500,
+        responsiveClass: true,
+        responsive:{
+            100:{
+                items: 1,
+            },
+
+            1400:{
+                item: 1.5,
+            },
+        }
+	});
+  });
