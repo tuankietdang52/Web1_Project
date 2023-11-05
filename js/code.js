eae4f7a28b50c1fc996eec113b2cfcf995e6d41e
tuankietdang52 <dkiet27@gmail.com>
2023-11-05 18:45:36 +0700

    add header

---
+++ js/code.js
@@ -0,0 +1,84 @@
+function addheader(){
+    document.write(`<header>
+    <section class="navigation-sect">
+        <div class="navigation-container">
+            <div class="contact-us">
+                <li>
+                    <a class="icon fb" href="#">
+                    </a>
+                </li>
+                <li>
+                    <a class="icon twitter" href="#">
+                    </a>
+                </li>
+                <li>
+                    <a class="icon google" href="#">
+                    </a>
+                </li>
+                <li>
+                    <a class="icon youtube" href="#">
+                    </a>
+                </li>
+            </div>
+            <div class="navigation">
+                <li>
+                    <a href="#" class="icon home">
+                        <p class="navi-text">Trang chủ</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="icon new">
+                        <p class="navi-text">Tin tức</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="icon recruit">
+                        <p class="navi-text">Tuyển dụng</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="icon info">
+                        <p class="navi-text">Giới thiệu</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="icon fix">
+                        <p class="navi-text">Bảo hành</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="icon contact">
+                        <p class="navi-text">Liên hệ</p>
+                    </a>
+                </li>
+            </div>
+        </div>
+    </section>
+    <section class="header-content">
+        <img src="../img/storybg.webp" alt="logo">
+        <div class="searchbar-container">
+            <div class="searchbox">
+                <input type="text" class="searchbar" placeholder="Search">
+                <button class="searchbutton">Tìm kiếm</button>
+            </div>
+            <div class="keyword">Từ khóa: 
+                <li><a href="#">Samsung</a></li>
+                <li><a href="#">Iphone</a></li>
+                <li><a href="#">Huawei</a></li>
+                <li><a href="#">Oppo</a></li>
+                <li><a href="#">Mobi</a></li>
+            </div>
+        </div>
+        <div class="user-option">
+            <a href="#" class="user-option-container">
+                <i class="icon account"></i>
+                Tài khoản
+            </a>
+            <a href="#" class="user-option-container">
+                <i class="icon cart"></i>
+                Giỏ hàng
+            </a>
+        </div>
+    </section>
+</header>`);
+}
