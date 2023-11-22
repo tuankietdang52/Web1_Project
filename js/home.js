276ca4958267cc8ca632f09d4264f503a97727bb
tuankietdang52 <dkiet27@gmail.com>
2023-11-23 01:12:17 +0700

    update chitietsanpham

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-// Slideshow by owl library //
+// Get Data for Product Frame //
 
 let frameindex = 0;
 let producthot = [];
@@ -8,7 +8,9 @@ let shockonlproduct = [];
 let productdiscount = [];
 let cheapproduct = [];
 
-function getData(){
+getPreviewData();
+
+function getPreviewData(){
     getProductHot();
     getNewProduct();
     getSaleProduct0();
@@ -17,7 +19,7 @@ function getData(){
     getCheapProduct();
 }
 
-function getProductHot(){;
+function getProductHot(){
     producthot = sortbyamountstar(2);
     producthot = sortbyratecount("desc", producthot);
 }
@@ -41,6 +43,8 @@ function getDiscountProduct(){
 function getCheapProduct(){
     cheapproduct = sortbyamountprice(3000000, "below");
 }
+
+// Slideshow by owl library //
 
 $(document).ready(function(){
     slideshowowl();
@@ -67,132 +71,6 @@ function slideshowowl(){
             },
         }
 	});
-}
-
-// VIET SAN PHAM VAO HTML //
-
-function writeproduct(product, sectionclassname){
-    let productcode = product[0].masp;
-    let nameproduct =  product[0].name;
-    let imgsrc = product[0].img;
-
-    let productsect = document.getElementsByClassName(sectionclassname)[0];
-
-    productsect.innerHTML += (`
-    <a href="#" class="product">
-        <img class="product-img" src="` + imgsrc + `" alt="` + productcode + `">
-        ` + writepromotag(product) + `
-        <span class="product-name">` + nameproduct + `</span>
-        <div class="product-price">` + 
-            writeprice(product) +
-        `</div>
-        <div class="star-container">` + addstarandratecount(product) + `</div>
-        <button class="addtocart-button icon"></button>
-    </a>
-    `);
-}
-
-function addstarandratecount(product){
-    let star = "";
-    let i = 0;
-    let amountstar = product[0].star;
-
-    while (i < 5){
-        if (i < amountstar) star += '<i class="fa fa-star"></i>';
-        else star += '<i class="fa fa-star-o"></i>';
-        i++;
-    }
-
-    let ratecount = product[0].rateCount;
-    let ratecounttag = `<span class="ratecountdisplay">` + ratecount + " đánh giá" + `</span>`;
-    star += ratecounttag;
-
-    return star;
-}
-
-function writeprice(product){
-    switch (product[0].promo.name){
-        case "giareonline":
-            return (`
-                <strong>` + product[0].promo.value +  `đ</strong>
-                <span>` + product[0].price + `đ<span>
-            `);
-
-        default:
-            return (`
-                <strong>` + product[0].price + `đ</strong>
-            `)
-    }
-}
-
-function writepromotag(product){
-    let promo = product[0].promo.name;
-    let promovalue = product[0].promo.value;
-
-    if (promo == "") return "";
-    return editpromotag(promo, promovalue);
-}
-
-function editpromotag(promo, value){
-    switch (promo){
-        case "moiramat":
-            return "<label class='promotag moiramat'>Mới ra mắt</label>";
-        case "tragop":
-            return "<label class='promotag tragop'>Trả góp " + value + "%</label>";
-        case "giareonline":
-            return "<label class='promotag giareonl'>Giá rẻ online</label>";
-        case "giamgia":
-            return (`
-                <label class='promotag giamgia'>
-                    <i class="fa-solid fa-bolt"></i>
-                    Giảm ` + value + `đ
-                </label>
-            `);
-    }
-}
-
-function writeamountremain(amount, link){
-    let productsect = document.getElementsByClassName("product-container")[frameindex];
-    productsect.innerHTML += (`
-        <a href="`+ link + `" class="see-all"></a>
-    `)
-    let seeallbutton = document.getElementsByClassName("see-all")[frameindex];
-    seeallbutton.innerHTML += (`
-         <h1>Xem tất cả <span>` + amount + `</span> sản phẩm</h1>
-    `)
-    frameindex++;
-}
-
-function getProductFrame(parentclassname){
-    switch (parentclassname){
-        case "product-hot":
-            return producthot;
-        case "new-product":
-            return newproduct;
-        case "sale-product-0":
-            return saleproduct0;
-        case "shock-price-online":
-            return shockonlproduct;
-        case "product-discount":
-            return productdiscount;
-        case "cheap-product":
-            return cheapproduct;
-        default:
-            return null;
-    }
-}
-
-// KET THUC VIET SAN PHAM //
-
-// viet khung san pham cua trang chu //
-function ReviewProduct(parentclassname, link = "#"){
-    let product = getProductFrame(parentclassname);
-    let productremain = product.length >= 5 ? product.length - 5 : 0;
-    for (let i = 0; i < 5; i++){
-        if (!product[i]) break;
-        writeproduct(product[i], parentclassname);
-    }
-    writeamountremain(productremain, link);
 }
 
 // viet filter cua cong ty vao html //
